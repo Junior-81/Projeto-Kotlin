1ff8b2d47612b47e9c4b529d914578a090c05879
eduardosdl <edu.silva.e08@gmail.com>
2024-06-07 18:48:17 -0300

    configure drawer navigation in main activity

--- app/src/main/java/com/example/coursestrack/MainActivity.kt
+++ app/src/main/java/com/example/coursestrack/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.coursestrack
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -21,15 +20,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHost.id) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.navHost.id) as NavHostFragment
         navController = navHostFragment.navController
 
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.navView.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            when(destination.id) {
-                R.id.homeFragment -> binding.toolbar.visibility = View.GONE
+            when (destination.id) {
                 R.id.loginFragment -> binding.toolbar.visibility = View.GONE
                 R.id.registerFragment -> binding.toolbar.visibility = View.GONE
                 else -> binding.toolbar.visibility = View.VISIBLE
